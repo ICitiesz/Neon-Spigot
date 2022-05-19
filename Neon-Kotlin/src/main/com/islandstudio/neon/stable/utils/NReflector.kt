1cb8caabd53733f8b5d3d3de0cb15a8a93c4e917
ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>
2022-05-19 22:05:20 +0800

    Update for v1.10-pre_2
    Signed-off-by: ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>

---
+++ Neon-Kotlin/src/main/com/islandstudio/neon/stable/utils/NReflector.kt
@@ -0,0 +1,29 @@
+package com.islandstudio.neon.stable.utils
+
+import com.islandstudio.neon.Neon
+import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.java.JavaPlugin.getPlugin
+
+object NReflector {
+    private val plugin: Plugin = getPlugin(Neon::class.java)
+
+    /**
+     * Get the Minecraft namespace class
+     *
+     * @param className The class name. (String)
+     * @return The Minecraft namespace class. (Class)
+     */
+    fun getNamespaceClass(className: String): Class<*> {
+        return Class.forName("net.minecraft.$className")
+    }
+
+    /**
+     * Get the Neon class
+     *
+     * @param className The class name. (String)
+     * @return The Neon class. (Class)
+     */
+    fun getNClassName(className: String): Class<*> {
+        return Class.forName("com.islandstudio.$className")
+    }
+}
