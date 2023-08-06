db2f42e6e326a8226380422ad00298d0124f5267
ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>
2023-08-06 15:02:15 +0800

    - Added functions to adapt custom option value. - Optimized core functions.
    Signed-off-by: ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>

--- Neon-Kotlin/src/main/com/islandstudio/neon/experimental/nBundle/NBundle.kt
+++ Neon-Kotlin/src/main/com/islandstudio/neon/experimental/nBundle/NBundle.kt
@@ -1,9 +1,9 @@
 package com.islandstudio.neon.experimental.nBundle
 
-import com.islandstudio.neon.Neon
+import com.islandstudio.neon.experimental.nServerFeatures.NServerFeatures
+import com.islandstudio.neon.experimental.nServerFeatures.ServerFeature
 import com.islandstudio.neon.stable.primary.nConstructor.NConstructor
-import com.islandstudio.neon.stable.primary.nExperimental.NExperimental
-import com.islandstudio.neon.stable.utils.NNamespaceKeys
+import com.islandstudio.neon.stable.utils.NeonKey
 import org.bukkit.Material
 import org.bukkit.entity.Villager
 import org.bukkit.event.EventHandler
@@ -14,58 +14,64 @@ import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.MerchantRecipe
 import org.bukkit.inventory.ShapedRecipe
 import org.bukkit.loot.LootTable
-import org.bukkit.plugin.Plugin
-import org.bukkit.plugin.java.JavaPlugin
 
 object NBundle {
-    private const val GENERATE_CHANCE: Double = 0.17
-    private val isEnabled: () -> Boolean = {
-        var tempBool = false
+    /* Default Bundle trade values */
+    private var bundleGenerateChance = 0.17
+    private var bundleMaxBuy = 12
+    private var bundlePrice = 5
+    private var bundlePriceMultiplier = 0.2f
+    private var villagerExperience = 5
 
-        NExperimental.Handler.getClientElement().forEach {
-            val nExperimental = NExperimental(it)
+    private val isEnabled = NServerFeatures.getToggle(ServerFeature.FeatureNames.N_BUNDLE)
 
-            if (!nExperimental.experimentalName.equals("nBundle", true)) return@forEach
-
-            if (!nExperimental.isEnabled) return@forEach
-
-            tempBool = true
-        }
-
-        tempBool
+    private enum class LootStructure(val key: String) {
+        VILLAGE_TANNERY("village_tannery"),
+        ABANDONED_MINESHAFT("abandoned_mineshaft"),
+        DESERT_PYRAMID("desert_pyramid")
     }
 
     /**
      * Initialize the nBundle.
      */
     fun run() {
-        if (!isEnabled()) {
-            NConstructor.plugin.server.worlds.forEach {
+        val server = NConstructor.plugin.server
+
+        if (!isEnabled) {
+            server.worlds.forEach {
                 it.entities.parallelStream()
                     .filter { entity -> entity is Villager }
-                    .filter { entity -> (entity as Villager).profession == Villager.Profession.LEATHERWORKER }.forEach { entity ->
+                    .filter { entity -> (entity as Villager).profession == Villager.Profession.LEATHERWORKER }.forEach FE@ { entity ->
                         val villager = entity as Villager
                         val villagerRecipes: ArrayList<MerchantRecipe> = ArrayList(villager.recipes)
 
-                        if (villagerRecipes.any { recipe -> recipe.result == ItemStack(Material.BUNDLE) }) {
-                            villagerRecipes.remove(villagerRecipes.first { recipe -> recipe .result == ItemStack(Material.BUNDLE) })
-                            villager.recipes = villagerRecipes
-                        }
+                        if (!villagerRecipes.any { recipe -> recipe.result == ItemStack(Material.BUNDLE) }) return@FE
+
+                        villagerRecipes.remove(villagerRecipes.first { recipe -> recipe .result == ItemStack(Material.BUNDLE) })
+                        villager.recipes = villagerRecipes
                 }
             }
 
-            return NConstructor.unRegisterEvent(EventController())
+            return NConstructor.unRegisterEvent(EventProcessor())
         }
 
-        NConstructor.registerEvent(EventController())
+        val featureName = ServerFeature.FeatureNames.N_BUNDLE.featureName
 
-        val shapedRecipe = ShapedRecipe(NNamespaceKeys.NEON_BUNDLE.key, ItemStack(Material.BUNDLE))
+        bundleGenerateChance = NServerFeatures.getOptionValue(featureName, "bundleGenerateChance" ) as Double
+        bundleMaxBuy = NServerFeatures.getOptionValue(featureName, "bundleMaxBuy") as Int
+        bundlePrice = NServerFeatures.getOptionValue(featureName, "bundlePrice") as Int
+        bundlePriceMultiplier = (NServerFeatures.getOptionValue(featureName, "bundlePriceMultiplier") as Double).toFloat()
+        villagerExperience = NServerFeatures.getOptionValue(featureName, "villagerExperience") as Int
+
+        NConstructor.registerEventProcessor(EventProcessor())
+
+        val shapedRecipe = ShapedRecipe(NeonKey.NamespaceKeys.NEON_BUNDLE.key, ItemStack(Material.BUNDLE))
 
         shapedRecipe.shape("SRS", "R R", "RRR")
         shapedRecipe.setIngredient('S', Material.STRING)
         shapedRecipe.setIngredient('R', Material.RABBIT_HIDE)
 
-        NConstructor.plugin.server.addRecipe(shapedRecipe)
+        server.addRecipe(shapedRecipe)
     }
 
     /**
@@ -76,14 +82,14 @@ object NBundle {
      *
      * @return The loot structure loot with bundle.
      */
-    private fun generateLoot(lootTable: LootTable, loot: ArrayList<ItemStack>): List<ItemStack> {
+    private fun generateBundle(lootTable: LootTable, loot: ArrayList<ItemStack>): List<ItemStack> {
         val lootTableKey: String = lootTable.key.toString()
 
         if (!lootTableKey.startsWith("minecraft:chests")) return loot
 
         if (!LootStructure.values().map { it.key }.contains(lootTableKey.split("/").last())) return loot
 
-        if (Math.random() <= GENERATE_CHANCE) {
+        if (Math.random() <= bundleGenerateChance) {
             loot.add(ItemStack(Material.BUNDLE, 1))
             return loot
         }
@@ -99,40 +105,41 @@ object NBundle {
     private fun setTradingRecipe(villager: Villager) {
         if (villager.profession != Villager.Profession.LEATHERWORKER) return
 
-        /* Bundle sale properties */
-        val maxBuy = 12
-        val normalPrice = 5
-        val priceMultiplier = 0.2f
-        val villagerExperience = 5
+        val villagerTradeContent: ArrayList<MerchantRecipe> = ArrayList(villager.recipes)
+        val villagerLevel = villager.villagerLevel
+        val bundleMerchantRecipe = MerchantRecipe(ItemStack(Material.BUNDLE), bundleMaxBuy)
 
-        val villagerRecipes: ArrayList<MerchantRecipe> = ArrayList(villager.recipes)
-        val bundleMerchantRecipe = MerchantRecipe(ItemStack(Material.BUNDLE), maxBuy)
-
-        bundleMerchantRecipe.addIngredient(ItemStack(Material.EMERALD, normalPrice))
-        bundleMerchantRecipe.setExperienceReward(true)
+        bundleMerchantRecipe.addIngredient(ItemStack(Material.EMERALD, bundlePrice))
         bundleMerchantRecipe.villagerExperience = villagerExperience
-        bundleMerchantRecipe.priceMultiplier = priceMultiplier
+        bundleMerchantRecipe.priceMultiplier = bundlePriceMultiplier
 
-        if (villagerRecipes.any { it.result == ItemStack(Material.BUNDLE) }) {
-            if (villager.villagerLevel >= 2) return
+        if (villagerTradeContent.any { it.result == ItemStack(Material.BUNDLE) }) {
+            if (villagerLevel >= 2) {
+                villagerTradeContent.find { it.result == ItemStack(Material.BUNDLE) }.let {
+                    villagerTradeContent[villagerTradeContent.indexOf(it)] = bundleMerchantRecipe
+                }
 
-            villagerRecipes.remove(villagerRecipes.first { it.result == ItemStack(Material.BUNDLE) })
+                villager.recipes = villagerTradeContent
+                return
+            }
 
-            villager.recipes = villagerRecipes
+            villagerTradeContent.remove(villagerTradeContent.first { it.result == ItemStack(Material.BUNDLE) })
+
+            villager.recipes = villagerTradeContent
             return
         }
 
-        if (villager.villagerLevel < 2) return
+        if (villagerLevel < 2) return
 
-        villagerRecipes.add(bundleMerchantRecipe)
+        villagerTradeContent.add(bundleMerchantRecipe)
 
-        villager.recipes = villagerRecipes
+        villager.recipes = villagerTradeContent
     }
 
-    private class EventController: Listener {
+    private class EventProcessor: Listener {
         @EventHandler
         private fun onLootGenerate(e: LootGenerateEvent) {
-            e.setLoot(generateLoot(e.lootTable, ArrayList(e.loot)))
+            e.setLoot(generateBundle(e.lootTable, ArrayList(e.loot)))
         }
 
         @EventHandler
@@ -141,11 +148,5 @@ object NBundle {
 
             setTradingRecipe(villager)
         }
-    }
-
-    private enum class LootStructure(val key: String) {
-        VILLAGE_TANNERY("village_tannery"),
-        ABANDONED_MINESHAFT("abandoned_mineshaft"),
-        DESERT_PYRAMID("desert_pyramid")
     }
 }
