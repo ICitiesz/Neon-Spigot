b7574eec36eeedfd286e430fb559f6c26efaa140
ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>
2023-08-13 00:59:27 +0800

    - Updated Enum from 'SERVER_CONFIG_FOLDER' to 'NSERVERFEATURES_FOLDER' - Changed 'NEXPERIMENTAL_FOLDER' folder path from 'MODE_FOLDER' to 'NSERVERFEATURES_FOLDER'
    Signed-off-by: ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>

--- Neon-Kotlin/src/main/com/islandstudio/neon/stable/primary/nFolder/FolderList.kt
+++ Neon-Kotlin/src/main/com/islandstudio/neon/stable/primary/nFolder/FolderList.kt
@@ -7,13 +7,12 @@ enum class FolderList(val folder: File) {
     /* All the required folders will be here */
     /* Stable */
     MODE_FOLDER(File(NFolder.getDataFolder(), "${NFolder.getVersion()}/${NServerConstantProcessor.getMode()}")),
-    SERVER_CONFIG_FOLDER(File(MODE_FOLDER.folder, "nServerFeatures")),
-    NEXPERIMENTAL_FOLDER(File(MODE_FOLDER.folder, "nExperimental")),
+    NSERVERFEATURES_FOLDER(File(MODE_FOLDER.folder, "nServerFeatures")),
+    NEXPERIMENTAL_FOLDER(File(NSERVERFEATURES_FOLDER.folder, "nExperimental")),
     NPROFILE(File(MODE_FOLDER.folder, "nProfile")),
-    NWAYPOINTS_FOLDER(File(SERVER_CONFIG_FOLDER.folder, "nWaypoints")),
+    NWAYPOINTS_FOLDER(File(NSERVERFEATURES_FOLDER.folder, "nWaypoints")),
 
     /* Experimental */
-    NSERVERFEATURES_FOLDER(File(NEXPERIMENTAL_FOLDER.folder, "nServerFeatures")),
     NFIREWORKS(File(NEXPERIMENTAL_FOLDER.folder, "nFireworks")),
     NFIREWORKS_IMAGES(File(NFIREWORKS.folder, "images")),
     NFIREWORKS_PARTICLE_DATA(File(NFIREWORKS.folder, "particle_data"))
