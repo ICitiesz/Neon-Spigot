3c422cf57eb6cd6c19c01775d378e4def25fe19e
ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>
2023-09-06 20:55:28 +0800

    - Added 'RecipeRegistry' interface where it provides functions to filter custom-made recipes with certain condition and register to the server.
    Signed-off-by: ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>

---
+++ Neon-Kotlin/src/main/com/islandstudio/neon/stable/core/RecipeRegistry.kt
@@ -0,0 +1,30 @@
+package com.islandstudio.neon.stable.core
+
+interface RecipeRegistry {
+    /**
+     * Filter recipe to meet the specific condition.
+     *
+     * @param recipeHolder
+     * @return
+     */
+    fun filterRecipe(recipeHolder: String): HashMap<String, NRecipes> {
+        val filteredRecipes: HashMap<String, NRecipes> = HashMap()
+
+        NRecipes.values().filter { nRecipe -> nRecipe.name.startsWith(recipeHolder, true) }.forEach { nRecipe ->
+            if (nRecipe.result.bukkitMaterial == null) return@forEach
+
+            if (nRecipe.ingredients.isEmpty()) return@forEach
+            nRecipe.ingredients.removeIf { ingredient -> ingredient.bukkitMaterial == null }
+
+            filteredRecipes[nRecipe.key.toString()] = nRecipe
+        }
+
+        return filteredRecipes
+    }
+
+    /**
+     * Register recipe to the server
+     *
+     */
+    fun registerRecipe()
+}
