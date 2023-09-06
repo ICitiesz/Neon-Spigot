3097542d62db8db329ab7583eb4a816523007b3c
ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>
2023-09-06 21:01:45 +0800

    - Added new recipes:   -> New wood variant [Mangrove, Cherry] for Charcoal   -> Block of raw metal [Raw Iron, Raw Copper, Raw Gold]   -> Cobbled Deepslate - Optimized implementation to satisfy with new recipes. - Removed 'Smeltable' enum due to all recipes has been migrated to 'NRecipes' enum
    Signed-off-by: ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>

--- Neon-Kotlin/src/main/com/islandstudio/neon/stable/secondary/nSmelter/NSmelter.kt
+++ Neon-Kotlin/src/main/com/islandstudio/neon/stable/secondary/nSmelter/NSmelter.kt
@@ -1,89 +1,72 @@
 package com.islandstudio.neon.stable.secondary.nSmelter
 
 import com.islandstudio.neon.Neon
+import com.islandstudio.neon.stable.core.NRecipes
+import com.islandstudio.neon.stable.core.RecipeRegistry
 import com.islandstudio.neon.stable.primary.nServerFeatures.NServerFeatures
 import com.islandstudio.neon.stable.primary.nServerFeatures.ServerFeature
-import org.bukkit.Material
-import org.bukkit.NamespacedKey
 import org.bukkit.inventory.BlastingRecipe
 import org.bukkit.inventory.FurnaceRecipe
 import org.bukkit.inventory.ItemStack
-import org.bukkit.inventory.Recipe
+import org.bukkit.inventory.RecipeChoice.MaterialChoice
 import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin.getPlugin
 
-object NSmelter {
+class NSmelter: RecipeRegistry {
     private val plugin: Plugin = getPlugin(Neon::class.java)
 
-    /**
-     * Initializes the nSmelter.
-     */
-    fun run() {
-        if (!NServerFeatures.getToggle(ServerFeature.FeatureNames.N_SMELTER)) return
+    object Handler {
+        /**
+         * Initializes the nSmelter.
+         */
+        fun run() {
+            if (!NServerFeatures.getToggle(ServerFeature.FeatureNames.N_SMELTER)) return
 
-        getSmeltableItems().keys.forEach { key ->
-            var furnaceRecipe: FurnaceRecipe? = null
-
-            val inputMaterial: Material? = getSmeltableItems()[key]?.get(0)
-            val resultMaterial: Material? = getSmeltableItems()[key]?.get(1)
-
-            if (resultMaterial == null || inputMaterial == null) return@forEach
-
-
-            val result = ItemStack(resultMaterial)
-            var namespacedKey = NamespacedKey(plugin, "neon_${result.type.name.lowercase()}")
-
-            val recipes: List<Recipe> = plugin.server.getRecipesFor(ItemStack(resultMaterial))
-
-            recipes.forEach { recipe ->
-                if (recipe.toString().contains("FurnaceRecipe")) {
-                    furnaceRecipe = recipe as FurnaceRecipe
-                }
-            }
-
-            if (key.startsWith("SAND") || key.startsWith("RED_SAND")) {
-                namespacedKey = NamespacedKey(plugin, "${inputMaterial.name.lowercase()}_glass")
-            }
-
-            if (key.endsWith("CHARCOAL")) {
-                namespacedKey = NamespacedKey(plugin, key.lowercase())
-            }
-
-            if (furnaceRecipe == null) return
-
-            val exp: Float = furnaceRecipe!!.experience
-            val cookingTime: Int = (furnaceRecipe!!.cookingTime / 2)
-
-            val blastingRecipe = BlastingRecipe(
-                namespacedKey,
-                result,
-                inputMaterial,
-                exp,
-                cookingTime
-            )
-
-            plugin.server.addRecipe(blastingRecipe)
+            NSmelter().registerRecipe()
         }
     }
 
-    /**
-     * Gets the smeltable items.
-     *
-     * @return The smeltable items. (Map<String, ArrayList<Material>>)
-     */
-    private fun getSmeltableItems(): MutableMap<String, ArrayList<Material?>> {
-        val combinedSmeltableItems: MutableMap<String, ArrayList<Material?>> = HashMap()
+    override fun registerRecipe() {
+        val filteredRecipes = filterRecipe("NSMELTER")
 
-        Smeltable.values().forEach {
-            val smeltableItems: ArrayList<Material?> = ArrayList()
+        if (filteredRecipes.isEmpty()) return
 
-            if (it.input != null && it.result != null) {
-                smeltableItems.add(it.input)
-                smeltableItems.add(it.result)
-                combinedSmeltableItems[it.name] = smeltableItems
+        filteredRecipes.values.forEach { nRecipe ->
+            lateinit var blastingRecipe: BlastingRecipe
+
+            val furnaceRecipe: FurnaceRecipe?
+
+            val namespacedKey = nRecipe.key
+            val result = ItemStack(nRecipe.result.bukkitMaterial!!)
+            val ingredients = MaterialChoice(nRecipe.ingredients.map { it.bukkitMaterial })
+            val resultRecipeInServer = plugin.server.getRecipesFor(result)
+
+            furnaceRecipe = resultRecipeInServer.find { recipe -> recipe.toString().contains("FurnaceRecipe") }?.let {
+                it as FurnaceRecipe
             }
-        }
 
-        return combinedSmeltableItems
+            var expDrop = furnaceRecipe?.experience ?: 0f
+            var smeltingTime = furnaceRecipe?.cookingTime?.div(2) ?: 0
+
+            if (furnaceRecipe == null) {
+                when (nRecipe) {
+                    NRecipes.NSMELTER_IRON_BLOCK, NRecipes.NSMELTER_COPPER_BLOCK -> {
+                        expDrop = (0.7f * 9)
+                        smeltingTime = 140
+                    }
+
+                    NRecipes.NSMELTER_GOLD_BLOCK -> {
+                        expDrop = (1f * 9)
+                        smeltingTime = 140
+                    }
+
+                    else -> { return@forEach }
+                }
+            }
+
+            blastingRecipe = BlastingRecipe(namespacedKey, result, ingredients, expDrop, smeltingTime)
+
+            plugin.server.addRecipe(blastingRecipe)
+        }
     }
 }
