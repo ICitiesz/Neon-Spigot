f4d43abe002c9a1e587dccd330e70eb0e6e77581
ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>
2023-09-19 09:39:12 +0800

    - Updates 'Handler' to be included for NRank, NCutter, NSmelter
    Signed-off-by: ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>

--- Neon-Kotlin/src/main/com/islandstudio/neon/stable/primary/nConstructor/NClassProperties.kt
+++ Neon-Kotlin/src/main/com/islandstudio/neon/stable/primary/nConstructor/NClassProperties.kt
@@ -1,8 +1,8 @@
 package com.islandstudio.neon.stable.primary.nConstructor
 
 import com.islandstudio.neon.experimental.nPVP.NPVP
+import com.islandstudio.neon.stable.core.io.nFolder.NFolder
 import com.islandstudio.neon.stable.primary.nCommand.NCommand
-import com.islandstudio.neon.stable.primary.nFolder.NFolder
 import com.islandstudio.neon.stable.primary.nProfile.NProfile
 import com.islandstudio.neon.stable.primary.nServerConstantEventProcessor.NServerConstantProcessor
 import com.islandstudio.neon.stable.primary.nServerFeatures.NServerFeatures
@@ -13,22 +13,26 @@ import com.islandstudio.neon.stable.secondary.nHarvest.NHarvest
 import com.islandstudio.neon.stable.secondary.nRank.NRank
 import com.islandstudio.neon.stable.secondary.nSmelter.NSmelter
 import com.islandstudio.neon.stable.secondary.nWaypoints.NWaypoints
+import com.islandstudio.neon.stable.utils.NeonKey
+import com.islandstudio.neon.stable.utils.reflection.NReflector
 
 object NClassProperties {
     enum class NClasses(val nClass: Class<*>) {
         /* Classes will be arranged according to the priority */
         N_FOLDER(NFolder::class.java),
         N_SERVER_FEATURES(NServerFeatures.Handler::class.java),
+        NEON_KEY(NeonKey.Handler::class.java),
+        N_REFLECTOR(NReflector.Handler::class.java),
         N_PROFILE(NProfile.Handler::class.java),
         N_SERVER_CONSTANT_PROCESSOR_HANDLER(NServerConstantProcessor.Handler::class.java),
         N_COMMAND_COMPANION(NCommand.Companion::class.java),
-        N_RANK(NRank::class.java),
+        N_RANK_HANDLER(NRank.Handler::class.java),
         N_PVP(NPVP::class.java),
         N_WAYPOINTS_HANDLER(NWaypoints.Handler::class.java),
         N_DURABLE_HANDLER(NDurable.Handler::class.java),
         N_HARVEST_HANDLER(NHarvest.Handler::class.java),
-        N_CUTTER(NCutter::class.java),
-        N_SMELTER(NSmelter::class.java),
+        N_CUTTER_HANDLER(NCutter.Handler::class.java),
+        N_SMELTER_HANDLER(NSmelter.Handler::class.java),
         N_BUNDLE_HANDLER(NBundle.Handler::class.java),
         //N_FIREWORKS(NFireworks.Handler::class.java)
     }
@@ -38,9 +42,9 @@ object NClassProperties {
         * If the run() method is in the nested class, the nClassName should include the nested class name
         * E.g: NDurable.Handler.run() | nClassName: NDurable.Handler
         * */
-        N_RANK("NRank"),
-        N_CUTTER("NCutter"),
-        N_SMELTER("NSmelter"),
+        N_RANK("NRank.Handler"),
+        N_CUTTER("NCutter.Handler"),
+        N_SMELTER("NSmelter.Handler"),
         N_BUNDLE("NBundle.Handler"),
         N_DURABLE("NDurable.Handler")
     }
