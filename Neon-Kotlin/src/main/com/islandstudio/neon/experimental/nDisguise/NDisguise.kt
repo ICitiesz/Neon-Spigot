3e248f349d3e4014049c9bfffed256fb327105c1
ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>
2023-09-19 09:54:44 +0800

    - Commented out un-use test code.
    Signed-off-by: ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>

--- Neon-Kotlin/src/main/com/islandstudio/neon/experimental/nDisguise/NDisguise.kt
+++ Neon-Kotlin/src/main/com/islandstudio/neon/experimental/nDisguise/NDisguise.kt
@@ -1,16 +1,8 @@
 package com.islandstudio.neon.experimental.nDisguise
 
-import it.unimi.dsi.fastutil.ints.IntList
-import net.minecraft.core.Registry
-import net.minecraft.network.protocol.game.ClientboundAddMobPacket
-import net.minecraft.network.protocol.game.ClientboundRemoveEntitiesPacket
 import net.minecraft.server.level.ServerPlayer
-import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
-import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
-import java.lang.reflect.Field
-import java.lang.reflect.Modifier
 
 object NDisguise {
 //    private fun processMessage(rank: String, playerName: String, message: String, onlinePlayers: Player) {
@@ -45,31 +37,31 @@ object NDisguise {
     }
 
     fun testPacket(player: Player) {
-        val handle: Any = player.javaClass.getMethod("getHandle").invoke(player)!!
-
-        val serverPlayer: ServerPlayer = handle as ServerPlayer
-
-        val addLivingEntityPacket = ClientboundAddMobPacket(serverPlayer)
-        val removeEntitiesPacket = ClientboundRemoveEntitiesPacket()
-        val intField: Field = removeEntitiesPacket.javaClass.getDeclaredField("a")
-        intField.isAccessible = true
-
-        val idField: Field = (serverPlayer as net.minecraft.world.entity.Entity).javaClass.getField("at")
-        idField.isAccessible = true
-        val id = idField.get(serverPlayer) as Int
-
-        //intField.set(removeEntitiesPacket, intArrayOf(id))
-        removeEntitiesPacket.entityIds.add(id)
-
-        //removeEntitiesPacket.entityIds.add((serverPlayer as LivingEntity))
-
-        val field: Field = addLivingEntityPacket.javaClass.getDeclaredField("c")
-        field.isAccessible = true
-        field.set(addLivingEntityPacket, 107)
-
-        handle.connection!!.send(removeEntitiesPacket)
-        handle.connection!!.send(addLivingEntityPacket)
-
-        println("test packet sent!")
+//        val handle: Any = player.javaClass.getMethod("getHandle").invoke(player)!!
+//
+//        val serverPlayer: ServerPlayer = handle as ServerPlayer
+//
+//        //val addLivingEntityPacket = ClientboundAddMobPacket(serverPlayer)
+//        val removeEntitiesPacket = ClientboundRemoveEntitiesPacket()
+//        val intField: Field = removeEntitiesPacket.javaClass.getDeclaredField("a")
+//        intField.isAccessible = true
+//
+//        val idField: Field = (serverPlayer as net.minecraft.world.entity.Entity).javaClass.getField("at")
+//        idField.isAccessible = true
+//        val id = idField.get(serverPlayer) as Int
+//
+//        //intField.set(removeEntitiesPacket, intArrayOf(id))
+//        removeEntitiesPacket.entityIds.add(id)
+//
+//        //removeEntitiesPacket.entityIds.add((serverPlayer as LivingEntity))
+//
+//        val field: Field = addLivingEntityPacket.javaClass.getDeclaredField("c")
+//        field.isAccessible = true
+//        field.set(addLivingEntityPacket, 107)
+//
+//        handle.connection!!.send(removeEntitiesPacket)
+//        handle.connection!!.send(addLivingEntityPacket)
+//
+//        println("test packet sent!")
     }
 }
