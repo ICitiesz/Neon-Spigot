0c4c036e8f0ffed47655d3d7154696a9d1b75b2c
ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>
2023-10-25 15:01:16 +0800

    - Decreased pixel display duration from 3s to 2s.
    Signed-off-by: ICitiesMaxQ <69842262+ICitiesMaxQ@users.noreply.github.com>

--- Neon-Kotlin/src/main/com/islandstudio/neon/experimental/nFireworks/DisplayPixel.kt
+++ Neon-Kotlin/src/main/com/islandstudio/neon/experimental/nFireworks/DisplayPixel.kt
@@ -5,7 +5,7 @@ import org.bukkit.World
 import org.bukkit.scheduler.BukkitRunnable
 
 class DisplayPixel(private val pixel: Pixel, private val world: World) : BukkitRunnable() {
-    private val durationInTicks = (3 * 20)
+    private val durationInTicks = (2 * 20)
     private var currentTicks = 0
 
     override fun run() {
